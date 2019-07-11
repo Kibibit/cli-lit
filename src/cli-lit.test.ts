ea9d54b50fa6d268e0ba6c8420e1cfe1154060be
Neil Kalman <neilkalman@gmail.com>
2019-07-11 15:46:58 +0300

    fix: remove tests for now

--- src/cli-lit.test.ts
+++
@@ -1,7 +0,0 @@
-import { cliLit } from './cli-lit';
-
-describe('test cli lit', () => {
-  it.todo('should reflect the correct version');
-  it.todo('should define functions without any decorators');
-  it.todo('should replace function name if cliName decorator exists');
-});
